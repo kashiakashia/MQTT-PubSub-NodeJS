319ca6f5f832038ac72acf5f05beaf642636c341
kashiakashia <37773120+kashiakashia@users.noreply.github.com>
2024-04-10 17:55:46 +0200

    front-end and server in node.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mqtt from "mqtt";
+import fs from "fs-extra";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
