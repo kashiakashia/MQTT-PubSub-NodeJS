f9fa619a2a2eb7e088bd4eecfc9015afb3a7e707
kashiakashia <37773120+kashiakashia@users.noreply.github.com>
2024-04-12 13:34:59 +0200

    Connecting to broker added

--- index.js
+++ index.js
@@ -3,13 +3,54 @@ import bodyParser from "body-parser";
 import mqtt from "mqtt";
 import fs from "fs-extra";
 
+//import { dirname } from "path";
+//import { fileURLToPath } from "url";
+
+//const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const app = express();
 const port = 3000;
+let status;
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function connectToBroker(req, res, next) {
+  const host = "wss://" + req.body["host"];
+  const port = req.body["port"];
+  const userID = req.body["username"];
+  const passwordID = req.body["password"];
+
+  console.log("jestem tu");
+  var options = {
+    host: host,
+    schema: "wss://",
+    path: "/mqtt",
+    port: port,
+    protocolId: "MQTT",
+    protocolVersion: 4,
+    username: userID,
+    password: passwordID,
+  };
+
+  const client = mqtt.connect(host, options);
+
+  client.on("connect", function () {
+    console.log("Client connected");
+    next();
+  });
+
+  next();
+}
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.use(connectToBroker);
+
+app.post("/submit", (req, res) => {
+  res.render("index.ejs", { connectionStatus: status });
 });
 
 app.listen(port, () => {
